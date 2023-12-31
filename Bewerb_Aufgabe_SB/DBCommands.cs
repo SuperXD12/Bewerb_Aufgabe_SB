22007a9450617b408fe5da8b8e37c2ef0ba16742
Soeren Baumann <soeren.baumann01@t-online.de>
2023-12-31 14:41:28 +0100

    started command list structure, swagger and db working

---
+++ Bewerb_Aufgabe_SB/DBCommands.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Bewerb_Aufgabe_SB
+{
+    public class DBCommands
+    {
+
+        /*using (var connection = new SqliteConnection("Data Source=hello.db"))
+{
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        SELECT name
+        FROM user
+        WHERE id = $id
+    ";
+    command.Parameters.AddWithValue("$id", id);
+
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var name = reader.GetString(0);
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
+        */
+
+
+
+        // smtp client after saying i wantto send email with new mail account:
+        //var smtpClient = new SmtpClient("smtp.gmail.com")
+        //{
+        //    Port = 587,
+        //    Credentials = new NetworkCredential("username", "password"),
+        //    EnableSsl = true,
+        //};
+
+        //smtpClient.Send("email", "recipient", "subject", "body");
+
+    }
+}
