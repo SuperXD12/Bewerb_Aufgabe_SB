c004c12afdf67e2ff22bc57e60aa128e22b8e740
Soeren Baumann <soeren.baumann01@t-online.de>
2024-01-01 16:28:41 +0100

    statuscode now fully implemented with http responses

--- Bewerb_Aufgabe_SB/Controllers/NotificationController.cs
+++ Bewerb_Aufgabe_SB/Controllers/NotificationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 
 namespace Bewerb_Aufgabe_SB.Controllers
 {
@@ -6,7 +7,10 @@ namespace Bewerb_Aufgabe_SB.Controllers
     [Route("[controller]")]
     public class NotificationController : ControllerBase
     {
-        
+
+
+        //returns HTTP Status Code 409 = Conflict if exists
+        //return Conflict("Conflict Error - Your inserted id was wrong");
     }
 
     public class StatusCodeController : ControllerBase
@@ -15,8 +19,45 @@ namespace Bewerb_Aufgabe_SB.Controllers
 
         public IActionResult GetStatusByID(int id)
         {
+            Statuscode currentStatus = Statuscode.Error;
+            using (var connection = new SqliteConnection("Data Source=sqlitedatabase_sb_bew.db"))
+            {
+                connection.Open();
 
-            return Ok(DBCommands.Checkstatus(id));
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                SELECT Status
+                FROM Notifications
+                WHERE id = $id
+                 ";
+                command.Parameters.AddWithValue("$id", id);
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var status = UInt16.Parse(reader.GetString(0));
+                            if (Enum.IsDefined(typeof(Statuscode), status))
+                            {
+                                currentStatus = (Statuscode)status;
+                            }
+                            //else automatically sending error status | this should be prevented by only correctly altering statuscodes 
+                            
+                        }
+                        return Ok(currentStatus.ToString());
+                    }
+                    else {
+                        return NotFound("404 Not Found | The given ID could not be found");
+                    }
+                       
+                }
+                
+            }
+            
         }
     }
+
+
 }
