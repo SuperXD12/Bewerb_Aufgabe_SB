274a9f8db5b2e3b4dfb082f963417240a08a7976
Soeren Baumann <soeren.baumann01@t-online.de>
2024-01-01 18:00:49 +0100

    delete and cancel fully functional

--- Bewerb_Aufgabe_SB/Controllers/NotificationController.cs
+++ Bewerb_Aufgabe_SB/Controllers/NotificationController.cs
@@ -67,7 +67,7 @@ namespace Bewerb_Aufgabe_SB.Controllers
                 int exe = command.ExecuteNonQuery();
                 if (exe <= 0)
                 {
-                    return BadRequest("Something went wrong while deleting the message");
+                    return NotFound("The given ID could not be found");
                 }
                 else
                 {
@@ -79,21 +79,80 @@ namespace Bewerb_Aufgabe_SB.Controllers
             }
         }
 
+        [HttpDelete("~/CancelNotification")]
+        public IActionResult CancelNotification(int id)
+        {
+            Statuscode currentStatus = Statuscode.Error;
+            using (var connection = new SqliteConnection("Data Source=sqlitedatabase_sb_bew.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                SELECT Status
+                FROM Notifications
+                WHERE id = $id
+                 ";
+                command.Parameters.AddWithValue("$id", id);
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var status = UInt16.Parse(reader.GetString(0));
+                            if (Enum.IsDefined(typeof(Statuscode), status))
+                            {
+                                currentStatus = (Statuscode)status;
+                            }
+                            //else automatically sending error status | this should be prevented by only correctly altering statuscodes 
+
+                        }
+                        
+                    }
+                    else
+                    {
+                        return NotFound("The given ID could not be found");
+                    }
+
+                }
+
+            }
+            if (currentStatus != Statuscode.New) { 
+                return Conflict("The message can not be canceled due to the status"+ currentStatus);
+            }
+
+            using (var connection = new SqliteConnection("Data Source=sqlitedatabase_sb_bew.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                            @"
+                DELETE FROM Notifications WHERE ID = $id
+                 ";
+                command.Parameters.AddWithValue("$id", id);
+
+                int exe = command.ExecuteNonQuery();
+                if (exe <= 0)
+                {
+                    return BadRequest("Something went wrong while deleting the message");
+                }
+                else
+                {
+                    return Ok("Message with the index " + id + " was successfully deleted");
+                }
+
+
+
+            }
+        }
     }
 
-    public class DeleteNotificationController : ControllerBase
-    {
-
-
-        
-    }
-
-    public class CancelNotificationController : ControllerBase
-    {
 
 
 
-    } 
 
     /*public static int GetPersonByName(string address) {
             
